74847bb99a3b82e4519d515159afce3d6dbdedad
Joe Huss <detain@interserver.net>
2017-06-07 11:09:18 -0400

    updates to the module

--- myadmin.php
+++ myadmin.php
@@ -5,21 +5,21 @@
  - add way to call/hook into install/uninstall
 */
 return [
-	'name' => 'Webhosting Licensing VPS Addon',
-	'description' => 'Allows selling of Webhosting Server and VPS License Types.  More info at https://www.netenberg.com/webhosting.php',
-	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a webhosting license. Allow 10 minutes for activation.',
-	'module' => 'vps',
+	'name' => 'MyAdmin Webhosting Module for MyAdmin',
+	'description' => 'Allows selling of Webhosting Module',
+	'help' => '',
+	'module' => 'webhosting',
 	'author' => 'detain@interserver.net',
-	'home' => 'https://github.com/detain/myadmin-webhosting-vps-addon',
-	'repo' => 'https://github.com/detain/myadmin-webhosting-vps-addon',
+	'home' => 'https://github.com/detain/myadmin-webhosting-module',
+	'repo' => 'https://github.com/detain/myadmin-webhosting-module',
 	'version' => '1.0.0',
-	'type' => 'addon',
+	'type' => 'module',
 	'hooks' => [
-		'vps.load_processing' => ['Detain\MyAdminWebhosting\Plugin', 'Load'],
+		'webhosting.load_processing' => ['Detain\MyAdminWebhosting\Plugin', 'Load'],
+		'webhosting.settings' => ['Detain\MyAdminWebhosting\Plugin', 'Settings'],
 		/* 'function.requirements' => ['Detain\MyAdminWebhosting\Plugin', 'Requirements'],
-		'licenses.settings' => ['Detain\MyAdminWebhosting\Plugin', 'Settings'],
-		'licenses.activate' => ['Detain\MyAdminWebhosting\Plugin', 'Activate'],
-		'licenses.change_ip' => ['Detain\MyAdminWebhosting\Plugin', 'ChangeIp'],
+		'webhosting.activate' => ['Detain\MyAdminWebhosting\Plugin', 'Activate'],
+		'webhosting.change_ip' => ['Detain\MyAdminWebhosting\Plugin', 'ChangeIp'],
 		'ui.menu' => ['Detain\MyAdminWebhosting\Plugin', 'Menu'] */
 	],
 ];
