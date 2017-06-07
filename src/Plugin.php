24a6b52ea72d2654470fdc71b048cf5fe39f5a77
Joe Huss <detain@interserver.net>
2017-06-07 11:15:12 -0400

    updates adding settings hook and basic stock setting

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,7 +10,11 @@ class Plugin {
 	}
 
 	public static function Load(GenericEvent $event) {
-	
+
 	}
 
+	public static function Settings(GenericEvent $event) {
+		$settings = $event->getSubject();
+		$settings->add_dropdown_setting('webhosting', 'General', 'outofstock_webhosting', 'Out Of Stock Webhosting', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_WEBHOSTING'), array('0', '1'), array('No', 'Yes', ));
+	}
 }
