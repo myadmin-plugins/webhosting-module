9220bdcae500e6ebd9512f54eebd3d17ce00a74e
Joe Huss <detain@interserver.net>
2017-06-24 06:44:06 -0400

    updates to module load payment processing call

--- src/Plugin.php
+++ src/Plugin.php
@@ -18,12 +18,12 @@ class Plugin {
 
 	public static function getHooks() {
 		return [
-			'webhosting.load_processing' => [__CLASS__, 'Load'],
+			'webhosting.load_processing' => [__CLASS__, 'loadProcessing'],
 			'webhosting.settings' => [__CLASS__, 'getSettings'],
 		];
 	}
 
-	public static function Load(GenericEvent $event) {
+	public static function loadProcessing(GenericEvent $event) {
 
 	}
 
